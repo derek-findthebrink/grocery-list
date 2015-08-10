41c2399a4cc5124e03efcbfac6252392a1f21db0
Derek <fiveflip@gmail.com>
2015-08-10 00:47:28 -0700

    tested on emulator, works well, will pull from firebase too

---
+++ platforms/android/assets/www/js/app/templates/pages/home.js
@@ -0,0 +1,9 @@
+define(['jadeRuntime'], function(jade) {
+return function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<h2>Still being built! Return when all widgets are completed, then widgetize!</h2>");;return buf.join("");
+};
+});
